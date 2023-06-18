af3043bc7b7e6a4a2bdd67d0e33f4cb297cdf13b
WALTAHHH <131562834+WALTAHHH@users.noreply.github.com>
2023-06-18 14:14:41 -0700

    Update _gameModal.tsx

--- pages/_gameModal.tsx
+++ pages/_gameModal.tsx
@@ -15,23 +15,24 @@ export default function GameModal({ home, away, homeScore, awayScore, logs }: { 
     <div className={ styles.wrapper }>
         <div className={ styles.content }>
             <div className={ styles.teamswrapper}>
-            <h1>
-                {home && home.name}
-                &nbsp;vs&nbsp;
-                {away && away.name}
-            </h1>
-            <h3>
-                {homeScore}
-                |
-                {awayScore}
-            </h3>
-            <div className={styles.playersScrollbar}>
-                {home?.players.map((p, i) => <Player p={p} key={i}/>)}
-            </div>
-            <div className={ styles.logs }>
-                {logs?.map((e, i) => <p key={i}>{e.content}</p>)}
+                <h1>
+                    {home && home.name}
+                    &nbsp;vs&nbsp;
+                    {away && away.name}
+                </h1>
+                <h3>
+                    {homeScore}
+                    |
+                    {awayScore}
+                </h3>
+                <div className={styles.playersScrollbar}>
+                    {home?.players.map((p, i) => <Player p={p} key={i}/>)}
+                </div>
+                <div className={ styles.logs }>
+                    {logs?.map((e, i) => <p key={i}>{e.content}</p>)}
+                </div>
             </div>
         </div>
     </div>
-    </>
+    </>;
 }
