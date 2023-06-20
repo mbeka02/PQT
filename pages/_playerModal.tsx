ab5eb5a4ffb47a7eb52c3846f9d651017a6cfaf1
WALTAHHH <131562834+WALTAHHH@users.noreply.github.com>
2023-06-20 15:43:23 -0700

    Update _playerModal.tsx

--- pages/_playerModal.tsx
+++ pages/_playerModal.tsx
@@ -16,6 +16,33 @@ const PlayerModal: React.FC<PlayerModalProps> = ({ player, hideModalOnClick }) =
   return (
     <div className={`${styles.playerModal} ${styles.modalTop}`}>
       <h2>{player.first_name} {player.last_name}</h2>
+		{player.Astral_Presence}<br/>
+		{player.Resilience}<br/>
+		{player.Radiance}<br/>
+		{player.Resonance}<br/>
+		{player.Dynamism}<br/>
+		{player.Vibes}<br/>
+		{player.Charm}<br/>
+		{player.Observation}<br/>
+		{player.Bravery}<br/>
+		{player.Creativity}<br/>
+		{player.Tenacity}<br/>
+		{player.Intelligence}<br/>
+		{player.Loyalty}<br/>
+		{player.Wit}<br/>
+		{player.Patience}<br/>
+		{player.Artistry}<br/>
+		{player.Technomancy}<br/>
+		{player.Gravity}<br/>
+		{player.Bioluminescence}<br/>
+		{player.Stink}<br/>
+		{player.Rhythm}<br/>
+		{player.Purple}<br/>
+		{player.Dankness}<br/>
+		{player.Savagery}<br/>
+		{player.Cleanliness}<br/>
+		{player.Unicorn}<br/>
+		{player.Thirst}<br/>
       {/* Add more content and functionality */}
       <button className={styles.closeButton} onClick={hideModalOnClick}>
         Close
