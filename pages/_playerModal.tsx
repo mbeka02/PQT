82f582e3a2c4fb9487fe91168cb44fde3d5fe864
WALTAHHH <131562834+WALTAHHH@users.noreply.github.com>
2023-06-20 15:50:09 -0700

    Update _playerModal.tsx

--- pages/_playerModal.tsx
+++ pages/_playerModal.tsx
@@ -16,33 +16,33 @@ const PlayerModal: React.FC<PlayerModalProps> = ({ player, hideModalOnClick }) =
   return (
     <div className={`${styles.playerModal} ${styles.modalTop}`}>
       <h2>{player.first_name} {player.last_name}</h2>
-		Astral_Presence: {player.Astral_Presence}<br/>
-		Resilience: {player.Resilience}<br/>
-		Radiance: {player.Radiance}<br/>
-		Resonance: {player.Resonance}<br/>
-		Dynamism: {player.Dynamism}<br/>
-		Vibes: {player.Vibes}<br/>
-		Charm: {player.Charm}<br/>
-		Observation: {player.Observation}<br/>
-		Bravery: {player.Bravery}<br/>
-		Creativity: {player.Creativity}<br/>
-		Tenacity: {player.Tenacity}<br/>
-		Intelligence: {player.Intelligence}<br/>
-		Loyalty: {player.Loyalty}<br/>
-		Wit: {player.Wit}<br/>
-		Patience: {player.Patience}<br/>
-		Artistry: {player.Artistry}<br/>
-		Technomancy: {player.Technomancy}<br/>
-		Gravity: {player.Gravity}<br/>
-		Bioluminescence: {player.Bioluminescence}<br/>
-		Stink: {player.Stink}<br/>
-		Rhythm: {player.Rhythm}<br/>
-		Purple: {player.Purple}<br/>
-		Dankness: {player.Dankness}<br/>
-		Savagery: {player.Savagery}<br/>
-		Cleanliness: {player.Cleanliness}<br/>
-		Unicorn: {player.Unicorn}<br/>
-		Thirst: {player.Thirst}<br/>
+		<strong>Astral Presence:</strong> {player.Astral_Presence}<br/>
+		<strong>Resilience:</strong> {player.Resilience}<br/>
+		<strong>Radiance:</strong> {player.Radiance}<br/>
+		<strong>Resonance:</strong> {player.Resonance}<br/>
+		<strong>Dynamism:</strong> {player.Dynamism}<br/>
+		<strong>Vibes:</strong> {player.Vibes}<br/>
+		<strong>Charm:</strong> {player.Charm}<br/>
+		<strong>Observation:</strong> {player.Observation}<br/>
+		<strong>Bravery:</strong> {player.Bravery}<br/>
+		<strong>Creativity:</strong> {player.Creativity}<br/>
+		<strong>Tenacity:</strong> {player.Tenacity}<br/>
+		<strong>Intelligence:</strong> {player.Intelligence}<br/>
+		<strong>Loyalty:</strong> {player.Loyalty}<br/>
+		<strong>Wit:</strong> {player.Wit}<br/>
+		<strong>Patience:</strong> {player.Patience}<br/>
+		<strong>Artistry:</strong> {player.Artistry}<br/>
+		<strong>Technomancy:</strong> {player.Technomancy}<br/>
+		<strong>Gravity:</strong> {player.Gravity}<br/>
+		<strong>Bioluminescence:</strong> {player.Bioluminescence}<br/>
+		<strong>Stink:</strong> {player.Stink}<br/>
+		<strong>Rhythm:</strong> {player.Rhythm}<br/>
+		<strong>Purple:</strong> {player.Purple}<br/>
+		<strong>Dankness:</strong> {player.Dankness}<br/>
+		<strong>Savagery:</strong> {player.Savagery}<br/>
+		<strong>Cleanliness:</strong> {player.Cleanliness}<br/>
+		<strong>Unicorn:</strong> {player.Unicorn}<br/>
+		<strong>Thirst:</strong> {player.Thirst}<br/>
       {/* Add more content and functionality */}
       <button className={styles.closeButton} onClick={hideModalOnClick}>
         Close
