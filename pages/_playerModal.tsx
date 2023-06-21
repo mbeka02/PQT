a40cb70b8de172ea9027bbc6d5cd1fd1184e6e49
WALTAHHH <131562834+WALTAHHH@users.noreply.github.com>
2023-06-21 10:36:05 -0700

    removed arrow functionality; updated arrow visuals

--- pages/_playerModal.tsx
+++ pages/_playerModal.tsx
@@ -16,45 +16,51 @@ const PlayerModal: React.FC<PlayerModalProps> = ({ players, hideModalOnClick }) 
 
   return (
     <div className={`${styles.playerModal} ${styles.modalTop}`}>
-      <h2>
-        {player.first_name} {player.last_name}
-      </h2>
-      <strong>Astral Presence:</strong> {player.Astral_Presence}          <br />
-      <strong>Resilience:</strong> {player.Resilience}                <br />
-      <strong>Radiance:</strong> {player.Radiance}                    <br />
-      <strong>Resonance:</strong> {player.Resonance}                  <br />
-      <strong>Dynamism:</strong> {player.Dynamism}                    <br />
-      <strong>Vibes:</strong> {player.Vibes}                          <br />      
-      <strong>Charm:</strong> {player.Charm}                          <br />
-      <strong>Observation:</strong> {player.Observation}              <br />
-      <strong>Bravery:</strong> {player.Bravery}                      <br />
-      <strong>Creativity:</strong> {player.Creativity}                <br />
-      <strong>Tenacity:</strong> {player.Tenacity}                    <br />
-      <strong>Intelligence:</strong> {player.Intelligence}            <br />
-      <strong>Loyalty:</strong> {player.Loyalty}                      <br />
-      <strong>Wit:</strong> {player.Wit}                              <br />
-      <strong>Patience:</strong> {player.Patience}                    <br />
-      <strong>Artistry:</strong> {player.Artistry}                    <br />
-      <strong>Technomancy:</strong> {player.Technomancy}              <br />
-      <strong>Gravity:</strong> {player.Gravity}                      <br />      
-      <strong>Bioluminescence:</strong> {player.Bioluminescence}      <br />
-      <strong>Stink:</strong> {player.Stink}                          <br />
-      <strong>Rhythm:</strong> {player.Rhythm}                        <br />
-      <strong>Purple:</strong> {player.Purple}                        <br />
-      <strong>Dankness:</strong> {player.Dankness}                    <br />
-      <strong>Savagery:</strong> {player.Savagery}                    <br />
-      <strong>Cleanliness:</strong> {player.Cleanliness}              <br />
-      <strong>Unicorn:</strong> {player.Unicorn}                      <br />
-      <strong>Thirst:</strong> {player.Thirst}                        <br />
+      <div className={styles.arrows}>
+          <button className={styles.arrowButton}>&larr;</button>
+          <button className={styles.arrowButton}>&rarr;</button>
+        </div>
 
-      <div>
-        <button disabled>&larr;</button> {/* left arrow */}
-        <button disabled>&rarr;</button> {/* right arrow */}
-      </div>
+        <div className={styles.playerInfo}>
+        <h2>
+          {player.first_name} {player.last_name}
+        </h2>
+        <strong>Astral Presence:</strong> {player.Astral_Presence}          <br />
+        <strong>Resilience:</strong> {player.Resilience}                <br />
+        <strong>Radiance:</strong> {player.Radiance}                    <br />
+        <strong>Resonance:</strong> {player.Resonance}                  <br />
+        <strong>Dynamism:</strong> {player.Dynamism}                    <br />
+        <strong>Vibes:</strong> {player.Vibes}                          <br />      
+        <strong>Charm:</strong> {player.Charm}                          <br />
+        <strong>Observation:</strong> {player.Observation}              <br />
+        <strong>Bravery:</strong> {player.Bravery}                      <br />
+        <strong>Creativity:</strong> {player.Creativity}                <br />
+        <strong>Tenacity:</strong> {player.Tenacity}                    <br />
+        <strong>Intelligence:</strong> {player.Intelligence}            <br />
+        <strong>Loyalty:</strong> {player.Loyalty}                      <br />
+        <strong>Wit:</strong> {player.Wit}                              <br />
+        <strong>Patience:</strong> {player.Patience}                    <br />
+        <strong>Artistry:</strong> {player.Artistry}                    <br />
+        <strong>Technomancy:</strong> {player.Technomancy}              <br />
+        <strong>Gravity:</strong> {player.Gravity}                      <br />      
+        <strong>Bioluminescence:</strong> {player.Bioluminescence}      <br />
+        <strong>Stink:</strong> {player.Stink}                          <br />
+        <strong>Rhythm:</strong> {player.Rhythm}                        <br />
+        <strong>Purple:</strong> {player.Purple}                        <br />
+        <strong>Dankness:</strong> {player.Dankness}                    <br />
+        <strong>Savagery:</strong> {player.Savagery}                    <br />
+        <strong>Cleanliness:</strong> {player.Cleanliness}              <br />
+        <strong>Unicorn:</strong> {player.Unicorn}                      <br />
+        <strong>Thirst:</strong> {player.Thirst}                        <br />
 
-      <button className={styles.closeButton} onClick={hideModalOnClick}>
-        Close
-      </button>
+        <div>
+          <button disabled>&larr;</button> {/* left arrow */}
+          <button disabled>&rarr;</button> {/* right arrow */}
+        </div>
+
+        <button className={styles.closeButton} onClick={hideModalOnClick}>
+          Close
+        </button>
     </div>
   );
 };
