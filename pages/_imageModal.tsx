1ca9f46739117ebd1f3f819bcf85b6db16036211
WALTAHHH <131562834+WALTAHHH@users.noreply.github.com>
2023-07-02 18:57:51 -0700

    Update _imageModal.tsx

--- pages/_imageModal.tsx
+++ pages/_imageModal.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import styles from 'styles/imageModal.module.css';
 
 interface ImageModalProps {
-  content: {
-    text: string;
-    imageSrc: string;
+  content?: {
+    text?: string;
+    imageSrc?: string;
   };
   onClose: () => void;
 }
 
 export default function ImageModal({ content, onClose }: ImageModalProps) {
+  const imageText = content?.text ?? 'No text available';
+  const imageSrc = content?.imageSrc;
+
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <h1>Log Item</h1>
-        <p>{content.text}</p>
-        <img src={content.imageSrc} alt="Game Log" />
+        <p>{imageText}</p>
+        {imageSrc && <img src={imageSrc} alt="Game Log" />} {/* Render the image only if imageSrc is available */}
         <button onClick={onClose}>Close</button>
       </div>
     </div>
