759da0124aef9da1242ceb8d941a2023f9866225
WALTAHHH <131562834+WALTAHHH@users.noreply.github.com>
2023-07-02 19:35:59 -0700

    Update _imageModal.tsx

--- pages/_imageModal.tsx
+++ pages/_imageModal.tsx
@@ -6,17 +6,19 @@ interface ImageModalProps {
     text?: string;
     imageSrc?: string;
   };
+  gameScore: string; // New prop for the game score
   onClose: () => void;
 }
 
-export default function ImageModal({ content, onClose }: ImageModalProps) {
+
+export default function ImageModal({ content, gameScore, onClose }: ImageModalProps) {
   const imageText = content?.text ?? 'No text available';
   const imageSrc = content?.imageSrc;
 
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        <h1>Game Log:</h1>
+        <h1>Game Log: {gameScore}</h1>
         <p>{imageText}</p>
         {imageSrc && <img src={imageSrc} alt="Game Log" />} {/* Render the image only if imageSrc is available */}
         <button onClick={onClose}>Close</button>
