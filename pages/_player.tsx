680c245310d5e647de058488696b244bc0a92737
mbeka02 <antonymbeka@gmail.com>
2023-07-19 15:08:13 +0300

    small UI fixes

--- pages/_player.tsx
+++ pages/_player.tsx
@@ -94,127 +94,127 @@ const PlayerView: React.FC<PlayerProps> = ({
             </div>
             <div className="grid md:grid-cols-custom_2  border-solid border-[1px] border-gray-300 w-3/4 justify-self-center md:w-full  ">
               <div className="flex flex-col py-2  border-solid border-r-[1px] border-gray-300 px-3">
-                <div className="font-semibold">
-                  Astral Presence:
-                  <span className=" font-normal ml-1">
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Astral Presence -
+                  <span className=" font-normal ml-1 ">
                     {player.Astral_Presence}
                   </span>
                 </div>
-                <div className="font-semibold">
-                  Resilience:{" "}
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Resilience -
                   <span className=" font-normal ml-1">{player.Resilience}</span>
                 </div>
-                <div className="font-semibold">
-                  Radiance:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Radiance -
                   <span className=" font-normal ml-1">{player.Radiance}</span>
                 </div>
-                <div className="font-semibold">
-                  Resonance:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Resonance -
                   <span className=" font-normal ml-1">{player.Resonance}</span>
                 </div>
-                <div className="font-semibold">
-                  Dynamism:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Dynamism -
                   <span className=" font-normal ml-1">{player.Dynamism}</span>
                 </div>
-                <div className="font-semibold">
-                  Vibes:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Vibes -
                   <span className=" font-normal ml-1">{player.Vibes} </span>
                 </div>
-                <div className="font-semibold">
-                  Charm:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Charm -
                   <span className=" font-normal ml-1">{player.Charm}</span>
                 </div>
-                <div className="font-semibold">
-                  Observation:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Observation -
                   <span className=" font-normal ml-1">
                     {player.Observation}
                   </span>
                 </div>
-                <div className="font-semibold">
-                  Bravery :
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Bravery -
                   <span className=" font-normal ml-1">{player.Bravery} </span>
                 </div>
-                <div className="font-semibold">
-                  Creativity:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Creativity -
                   <span className=" font-normal ml-1">{player.Creativity}</span>
                 </div>
-                <div className="font-semibold">
-                  Tenacity:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Tenacity -
                   <span className=" font-normal ml-1">{player.Tenacity}</span>
                 </div>
-                <div className="font-semibold">
-                  Intelligence:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Intelligence-
                   <span className=" font-normal ml-1">
                     {player.Intelligence}
                   </span>
                 </div>
-                <div className="font-semibold">
-                  Loyalty:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Loyalty-
                   <span className=" font-normal ml-1">{player.Loyalty}</span>
                 </div>
-                <div className="font-semibold">
-                  Wit:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Wit-
                   <span className=" font-normal ml-1">{player.Wit} </span>
                 </div>
               </div>
               <div className="flex flex-col py-2 px-3">
-                <div className="font-semibold">
-                  Patience:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Patience-
                   <span className=" font-normal ml-1">{player.Patience}</span>
                 </div>
-                <div className="font-semibold">
-                  Artistry:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Artistry-
                   <span className=" font-normal ml-1">{player.Artistry}</span>
                 </div>
-                <div className="font-semibold">
-                  Technomancy:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Technomancy-
                   <span className=" font-normal ml-1">
                     {player.Technomancy}
                   </span>
                 </div>
-                <div className="font-semibold">
-                  Gravity:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Gravity-
                   <span className=" font-normal ml-1">{player.Gravity} </span>
                 </div>
-                <div className="font-semibold">
-                  Bioluminescence:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Bioluminescence-
                   <span className=" font-normal ml-1">
                     {player.Bioluminescence}
                   </span>
                 </div>
 
-                <div className="font-semibold">
-                  Stink:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Stink-
                   <span className=" font-normal ml-1">{player.Stink}</span>
                 </div>
-                <div className="font-semibold">
-                  Rhythm:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Rhythm-
                   <span className=" font-normal ml-1">{player.Rhythm}</span>
                 </div>
-                <div className="font-semibold">
-                  Purple:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Purple-
                   <span className=" font-normal ml-1">{player.Purple} </span>
                 </div>
-                <div className="font-semibold">
-                  Dankness:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Dankness-
                   <span className=" font-normal ml-1">{player.Dankness}</span>
                 </div>
-                <div className="font-semibold">
-                  Savagery:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  <span>Savagery-</span>
                   <span className=" font-normal ml-1">{player.Savagery}</span>
                 </div>
-                <div className="font-semibold">
-                  Cleanliness:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Cleanliness-
                   <span className=" font-normal ml-1">
                     {player.Cleanliness}
                   </span>
                 </div>
-                <div className="font-semibold">
-                  Unicorn:
+                <div className="font-semibold  flex  w-3/4 justify-between">
+                  Unicorn-
                   <span className=" font-normal ml-1">{player.Unicorn} </span>
                 </div>
-                <div className="font-semibold">
-                  Thirst:
+                <div className="font-semibold flex  w-3/4 justify-between">
+                  Thirst-
                   <span className=" font-normal ml-1">{player.Thirst} </span>
                 </div>
               </div>
